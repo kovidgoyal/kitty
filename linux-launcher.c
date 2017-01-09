a2b2a2546bd48b9904a926a45e1aa39b089374a6
Kovid Goyal <kovid@kovidgoyal.net>
2017-01-09 12:16:37 +0530

    Print out an error message if launching python3 fails

--- linux-launcher.c
+++ linux-launcher.c
@@ -10,6 +10,8 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <string.h>
+#include <errno.h>
 
 #define MIN(x, y) ((x) < (y)) ? (x) : (y)
 #define MAX_ARGC 1024
@@ -18,16 +20,17 @@ int main(int argc, char *argv[]) {
 		char exe[PATH_MAX+1] = {0};
 		char lib[PATH_MAX+1] = {0};
 		char *final_argv[MAX_ARGC + 1] = {0};
-		if (realpath("/proc/self/exe", exe) == NULL) { fprintf(stderr, "Failed to read /proc/self/exe"); return 1; }
+		if (realpath("/proc/self/exe", exe) == NULL) { fprintf(stderr, "Failed to read /proc/self/exe\n"); return 1; }
 		char *exe_dir = dirname(exe);
 		int num = snprintf(lib, PATH_MAX, "%s%s", exe_dir, "/../lib/kitty");
-		if (num < 0 || num >= PATH_MAX) { fprintf(stderr, "Failed to create path to /../lib/kitty"); return 1; }
+		if (num < 0 || num >= PATH_MAX) { fprintf(stderr, "Failed to create path to /../lib/kitty\n"); return 1; }
 		final_argv[0] = "python3";
 		final_argv[1] = lib;
 		for (int i = 1; i < argc && i + 1 <= MAX_ARGC; i++) {
 				final_argv[i+1] = argv[i];
 		}
 		execvp(final_argv[0], final_argv);
+		fprintf(stderr, "Failed to run python3: %s\n", strerror(errno));
 		return 0;
 }
 
