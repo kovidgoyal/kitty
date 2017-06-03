419f43ceed7a1f176ff1bf1dba6c9a777d8eaf06
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-03 08:58:11 +0530

    Fix deprecation warning on macOS 10.12

--- kitty/cocoa_window.m
+++ kitty/cocoa_window.m
@@ -9,11 +9,15 @@
 #include "data-types.h"
 #include <Cocoa/Cocoa.h>
 
+#ifndef NSWindowStyleMaskTitled
+#define NSWindowStyleMaskTitled NSTitledWindowMask 
+#endif
+
 PyObject*
 cocoa_hide_titlebar(PyObject UNUSED *self, PyObject *window_id) {
     NSView *native_view = (NSView*)PyLong_AsVoidPtr(window_id);
     NSWindow* window = [native_view window];
     [window setStyleMask:
-        [window styleMask] & ~NSTitledWindowMask];
+        [window styleMask] & ~NSWindowStyleMaskTitled];
     Py_RETURN_NONE;
 }
