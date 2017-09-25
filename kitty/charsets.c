967e65e1502281a132011a1ff77d305a92008a9f
Kovid Goyal <kovid@kovidgoyal.net>
2017-09-25 17:30:11 +0530

    Use a static b64 decoding table

--- kitty/charsets.c
+++ kitty/charsets.c
@@ -267,24 +267,14 @@ encode_utf8(uint32_t ch, char* dest) {
 
 
 // Base64
-
-static uint8_t b64_encoding_table[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3',
-    '4', '5', '6', '7', '8', '9', '+', '/'
+// standard decoding using + and / with = being the padding character
+static uint8_t b64_decoding_table[256] = {
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 62, 0, 0, 0, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0, 0, 0, 0, 0, 0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
-
-static uint8_t b64_decoding_table[256] = {0};
 
 
 const char*
 base64_decode(const uint32_t *src, size_t src_sz, uint8_t *dest, size_t dest_capacity, size_t *dest_sz) {
-    if (!b64_decoding_table['B']) { for (uint8_t i = 0; i < sizeof(b64_encoding_table)/sizeof(b64_encoding_table[0]); i++) b64_decoding_table[b64_encoding_table[i]] = i; }
     if (!src_sz) { *dest_sz = 0; return NULL; }
     if (src_sz % 4 != 0) return "base64 encoded data must have a length that is a multiple of four";
     *dest_sz = (src_sz / 4) * 3;
@@ -292,10 +282,10 @@ base64_decode(const uint32_t *src, size_t src_sz, uint8_t *dest, size_t dest_cap
     if (src[src_sz - 2] == '=') (*dest_sz)--;
     if (*dest_sz > dest_capacity) return "output buffer too small";
     for (size_t i = 0, j = 0; i < src_sz;) {
-        uint32_t sextet_a = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++]];
-        uint32_t sextet_b = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++]];
-        uint32_t sextet_c = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++]];
-        uint32_t sextet_d = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++]];
+        uint32_t sextet_a = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++] & 0xff];
+        uint32_t sextet_b = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++] & 0xff];
+        uint32_t sextet_c = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++] & 0xff];
+        uint32_t sextet_d = src[i] == '=' ? 0 & i++ : b64_decoding_table[src[i++] & 0xff];
         uint32_t triple = (sextet_a << 3 * 6) + (sextet_b << 2 * 6) + (sextet_c << 1 * 6) + (sextet_d << 0 * 6);
 
         if (j < *dest_sz) dest[j++] = (triple >> 2 * 8) & 0xFF;
