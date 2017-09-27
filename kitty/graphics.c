2786976c63fe28ed5ef12587e5400ec414b80a19
Kovid Goyal <kovid@kovidgoyal.net>
2017-09-27 20:23:35 +0530

    ...

--- kitty/graphics.c
+++ kitty/graphics.c
@@ -432,7 +432,7 @@ W(image_for_client_id) {
     unsigned long id = PyLong_AsUnsignedLong(args);
     bool existing = false;
     Image *img = find_or_create_image(self, id, &existing);
-    if (existing) { Py_RETURN_NONE; }
+    if (!existing) { Py_RETURN_NONE; }
     return image_as_dict(img);
 }
 
