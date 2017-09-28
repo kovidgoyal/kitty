7da08d37d259bfbeca85d0bcf88bb54db21a3c39
Kovid Goyal <kovid@kovidgoyal.net>
2017-09-28 09:33:53 +0530

    File to host graphics tests

---
+++ kitty_tests/graphics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPL v3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
+
+import os
+
+from . import BaseTest
+
+
+def img_path(name):
+    base = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(base, name)
+
+
+class TestGraphics(BaseTest):
+
+    def test_load_images(self):
+        s = self.create_screen()
+        c = s.callbacks
+        g = s.grman
+        print(g)
