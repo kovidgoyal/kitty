625798220a7743a0e28ba3182cd7642c2cac6c34
Kovid Goyal <kovid@kovidgoyal.net>
2017-11-15 09:16:18 +0530

    ...

--- kitty/glfw.c
+++ kitty/glfw.c
@@ -181,7 +181,7 @@ create_new_os_window(PyObject UNUSED *self, PyObject *args) {
         glfwWindowHint(GLFW_SAMPLES, 0);
         glfwSwapInterval(0);  // a value of 1 makes mouse selection laggy
 #ifdef __APPLE__
-        if (OPT(macos_hide_titlebar)) glfwWindowHint(GLFW_DECORATED, False)
+        if (OPT(macos_hide_titlebar)) glfwWindowHint(GLFW_DECORATED, false)
         // OS X cannot handle 16bit stencil buffers
         glfwWindowHint(GLFW_STENCIL_BITS, 8)
 #else
