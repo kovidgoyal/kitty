59243355f6a1168b69887b8ab43752114f39887e
Kovid Goyal <kovid@kovidgoyal.net>
2017-11-27 15:37:13 +0530

    ...

--- kitty/cli.py
+++ kitty/cli.py
@@ -245,7 +245,10 @@ def wrap(text, limit=80):
 
 def print_help_for_seq(seq):
     from kitty.icat import screen_size
-    linesz = min(screen_size().cols, 76)
+    try:
+        linesz = min(screen_size().cols, 76)
+    except EnvironmentError:
+        linesz = 76
     blocks = []
     a = blocks.append
 
