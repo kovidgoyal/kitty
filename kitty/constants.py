eed11361c511b84ce8b12fa6940fb8e664e2d374
Kovid Goyal <kovid@kovidgoyal.net>
2017-12-18 06:59:19 +0530

    Fix typo in config directory override env var

--- kitty/constants.py
+++ kitty/constants.py
@@ -22,9 +22,8 @@ WindowGeometry = namedtuple('WindowGeometry', 'left top right bottom xnum ynum')
 
 
 def _get_config_dir():
-    # This must be called before calling setApplicationName
     if 'KITTY_CONFIG_DIRECTORY' in os.environ:
-        return os.path.abspath(os.path.expanduser(os.environ['VISE_CONFIG_DIRECTORY']))
+        return os.path.abspath(os.path.expanduser(os.environ['KITTY_CONFIG_DIRECTORY']))
 
     candidate = os.path.abspath(os.path.expanduser(os.environ.get('XDG_CONFIG_HOME') or ('~/Library/Preferences' if is_macos else '~/.config')))
     ans = os.path.join(candidate, appname)
