e716f6d35e2e6123687da1f447dede37a5ee7fa8
Kovid Goyal <kovid@kovidgoyal.net>
2018-03-04 12:28:06 +0530

    Replace deprecated ASL with os_log on macOS

--- kitty/logging.c
+++ kitty/logging.c
@@ -6,28 +6,48 @@
  */
 
 #include "data-types.h"
+#include <stdlib.h>
 #include <stdarg.h>
 #include <time.h>
 #include <sys/time.h>
+#ifdef __APPLE__
+#include <os/log.h>
+#endif
 
+
+static bool use_os_log = false;
 
 void
 log_error(const char *fmt, ...) {
     va_list ar;
     struct timeval tv;
-    gettimeofday(&tv, NULL);
-    struct tm *tmp = localtime(&tv.tv_sec);
-    if (tmp) {
-        char tbuf[256], buf[256];
-        if (strftime(buf, sizeof(buf), "%j %H:%M:%S.%%06u", tmp) != 0) {
-            snprintf(tbuf, sizeof(tbuf), buf, tv.tv_usec);
-            fprintf(stderr, "[%s] ", tbuf);
+#ifdef __APPLE__
+    // Apple does not provide a varargs style os_logv
+    char logbuf[16 * 1024] = {0};
+#else
+    char logbuf[4];
+#endif
+    char *p = logbuf;
+#define bufprint(func, ...) { if ((size_t)(p - logbuf) < sizeof(logbuf) - 2) { p += func(p, sizeof(logbuf) - (p - logbuf), __VA_ARGS__); } }
+    if (!use_os_log) {  // Apple's os_log already records timestamps
+        gettimeofday(&tv, NULL);
+        struct tm *tmp = localtime(&tv.tv_sec);
+        if (tmp) {
+            char tbuf[256] = {0}, buf[256] = {0};
+            if (strftime(buf, sizeof(buf), "%j %H:%M:%S.%%06u", tmp) != 0) {
+                snprintf(tbuf, sizeof(tbuf), buf, tv.tv_usec);
+                fprintf(stderr, "[%s] ", tbuf);
+            }
         }
     }
     va_start(ar, fmt);
-    vfprintf(stderr, fmt, ar);
+    if (use_os_log) { bufprint(vsnprintf, fmt, ar); }
+    else vfprintf(stderr, fmt, ar);
     va_end(ar);
-    fprintf(stderr, "\n");
+#ifdef __APPLE__
+    if (use_os_log) os_log(OS_LOG_DEFAULT, "%{public}s", logbuf);
+#endif
+    if (!use_os_log) fprintf(stderr, "\n");
 }
 
 static PyObject*
@@ -46,5 +66,8 @@ static PyMethodDef module_methods[] = {
 bool
 init_logging(PyObject *module) {
     if (PyModule_AddFunctions(module, module_methods) != 0) return false;
+#ifdef __APPLE__
+    if (getenv("KITTY_LAUNCHED_BY_LAUNCH_SERVICES") != NULL) use_os_log = true;
+#endif
     return true;
 }
