5a905dfe7f901fa826ee6e9ad9dbf5daa1b53253
Kovid Goyal <kovid@kovidgoyal.net>
2018-03-14 12:10:15 +0530

    Add kitty.app to .gitignore
    Also have make clean clean linux-package and kitty.app when running
    manually.

--- setup.py
+++ setup.py
@@ -619,7 +619,7 @@ def clean():
                 else:
                     os.unlink(x)
 
-    safe_remove('build', 'compile_commands.json')
+    safe_remove('build', 'compile_commands.json', 'linux-package', 'kitty.app')
     for root, dirs, files in os.walk('.'):
         remove_dirs = {d for d in dirs if d == '__pycache__'}
         [(shutil.rmtree(os.path.join(root, d)), dirs.remove(d)) for d in remove_dirs]
