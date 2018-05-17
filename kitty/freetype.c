2a99ef254f287197d6c49f60bdc3d9cbb788faa6
Kovid Goyal <kovid@kovidgoyal.net>
2018-05-17 19:49:31 +0530

    Dont segfault when printing a freetype font object with a NULL postscript name

--- kitty/freetype.c
+++ kitty/freetype.c
@@ -260,10 +260,11 @@ dealloc(Face* self) {
 
 static PyObject *
 repr(Face *self) {
+    const char *ps_name = FT_Get_Postscript_Name(self->face);
     return PyUnicode_FromFormat(
         "Face(family=%s, style=%s, ps_name=%s, path=%S, index=%d, is_scalable=%S, has_color=%S, ascender=%i, descender=%i, height=%i, underline_position=%i, underline_thickness=%i)",
         self->face->family_name ? self->face->family_name : "", self->face->style_name ? self->face->style_name : "",
-        FT_Get_Postscript_Name(self->face),
+        ps_name ? ps_name: "",
         self->path, self->index, self->is_scalable ? Py_True : Py_False, self->has_color ? Py_True : Py_False,
         self->ascender, self->descender, self->height, self->underline_position, self->underline_thickness
     );
