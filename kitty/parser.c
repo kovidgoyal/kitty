249258121b85fe1daaac3c1320802188041757e9
Dominique Martinet <asmadeus@codewreck.org>
2018-09-19 13:33:25 +0900

    parser: accept `reset` sequence to exit accumulate functions
    Input looks stuck until the accumulate buffer fills up; after displaying
    random bytes (e.g. cat a binary) nothing would print anymore but input
    would still work.
    
    In that situation, one would normally expect the `reset` command to "fix
    things" - so getting out of the accumulate loop with this sequence would
    make sense.
    
    I'd actually be tempted to bail out even more easily (e.g. when getting
    color codes as well, so a colorful PS1 prompt would already get out of
    the loop) but it might not really go well with the logic behind these
    accumulating sequences.
    
    You can try with `echo -e '\xc2\x9e'` for PM

--- kitty/parser.c
+++ kitty/parser.c
@@ -905,6 +905,7 @@ accumulate_osc(Screen *screen, uint32_t ch, PyObject DUMP_UNUSED *dump_callback)
         case DEL:
             break;
         case ESC_ST:
+        case ESC_RIS:
             if (screen->parser_buf_pos > 0 && screen->parser_buf[screen->parser_buf_pos - 1] == ESC) {
                 screen->parser_buf_pos--;
                 return true;
@@ -957,6 +958,7 @@ accumulate_oth(Screen *screen, uint32_t ch, PyObject DUMP_UNUSED *dump_callback)
         case ST:
             return true;
         case ESC_ST:
+        case ESC_RIS:
             if (screen->parser_buf_pos > 0 && screen->parser_buf[screen->parser_buf_pos - 1] == ESC) {
                 screen->parser_buf_pos--;
                 return true;
