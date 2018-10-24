0c0ce4844c9746cf0f3755de852cdd69f5f1204e
Kovid Goyal <kovid@kovidgoyal.net>
2018-10-24 07:07:57 +0530

    Dont use deprecated FT_Bitmap_New

--- kitty/freetype.c
+++ kitty/freetype.c
@@ -236,7 +236,7 @@ load_glyph(Face *self, int glyph_index, int load_type) {
     // Embedded bitmap glyph?
     if (self->face->glyph->bitmap.pixel_mode == FT_PIXEL_MODE_MONO && load_type != FT_LOAD_DEFAULT) {
         FT_Bitmap bitmap;
-        FT_Bitmap_New(&bitmap);
+        FT_Bitmap_Init(&bitmap);
 
         // This also sets pixel_mode to FT_PIXEL_MODE_GRAY so we don't have to
         error = FT_Bitmap_Convert(library, &self->face->glyph->bitmap, &bitmap, 1);
