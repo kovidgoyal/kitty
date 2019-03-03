13254ac4d5029dd5a850f00dcf66147e07be32d1
Kovid Goyal <kovid@kovidgoyal.net>
2019-03-03 07:24:05 +0530

    Fix #1435

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import importlib
 import json
 import os
 import re
+import runpy
 import shlex
 import shutil
 import subprocess
@@ -635,16 +636,11 @@ def package(args, for_bundle=False, sh_launcher=False):
     if os.path.exists(libdir):
         shutil.rmtree(libdir)
     os.makedirs(os.path.join(libdir, 'logo'))
+    build_terminfo = runpy.run_path('build-terminfo', run_name='import_build')
     for x in (libdir, os.path.join(ddir, 'share')):
         odir = os.path.join(x, 'terminfo')
         safe_makedirs(odir)
-        proc = subprocess.run(['tic', '-x', '-o' + odir, 'terminfo/kitty.terminfo'], check=True, stderr=subprocess.PIPE)
-        regex = '^"terminfo/kitty.terminfo", line [0-9]+, col [0-9]+, terminal \'xterm-kitty\': older tic versions may treat the description field as an alias$'
-        for error in proc.stderr.decode('utf-8').splitlines():
-            if not re.match(regex, error):
-                print(error, file=sys.stderr)
-        if not glob.glob(os.path.join(odir, '*/xterm-kitty')):
-            raise SystemExit('tic failed to output the compiled kitty terminfo file')
+        build_terminfo['compile_terminfo'](odir)
     shutil.copy2('__main__.py', libdir)
     shutil.copy2('logo/kitty.rgba', os.path.join(libdir, 'logo'))
     shutil.copy2('logo/kitty.png', os.path.join(libdir, 'logo'))
