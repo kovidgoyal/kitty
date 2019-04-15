beb90f2c550676db9c4b7170ba5fd9617540ece0
Luflosi <luflosi@luflosi.de>
2019-04-15 09:17:31 +0200

    Implement feedback

--- kitty/config_data.py
+++ kitty/config_data.py
@@ -445,11 +445,9 @@ o('click_interval', -1.0, option_type=float, long_text=_('''
 The interval between successive clicks to detect double/triple clicks (in seconds).
 Negative numbers will use the system default instead, if available, or fallback to 0.5.'''))
 
-o('mouse_hide_wait', 3.0, option_type=positive_float, long_text=_('''
+o('mouse_hide_wait', 3.0, option_type=float, long_text=_('''
 Hide mouse cursor after the specified number of seconds
-of the mouse not being used. Set to zero to disable mouse cursor hiding.'''))
-
-o('mouse_hide_key', True, long_text=_('''TODO'''))
+of the mouse not being used. Set to zero to disable mouse cursor hiding. TODO'''))
 
 o('focus_follows_mouse', False, long_text=_('''
 Set the active window to the window under the mouse when
--- kitty/state.h
+++ kitty/state.h
@@ -14,7 +14,7 @@ typedef enum { LEFT_EDGE, TOP_EDGE, RIGHT_EDGE, BOTTOM_EDGE } Edge;
 
 typedef struct {
     double visual_bell_duration, cursor_blink_interval, cursor_stop_blinking_after, mouse_hide_wait, click_interval, wheel_scroll_multiplier, touch_scroll_multiplier;
-    bool mouse_hide_key, enable_audio_bell;
+    bool enable_audio_bell;
     CursorShape cursor_shape;
     unsigned int open_url_modifiers;
     unsigned int rectangle_select_modifiers;
