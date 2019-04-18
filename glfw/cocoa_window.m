5c4462281ef644d30d118893e5c9afcb998d3842
Kovid Goyal <kovid@kovidgoyal.net>
2019-04-18 09:58:09 +0530

    Cocoa: Upstream fix for mouse hover detection
    https://github.com/glfw/glfw/commit/0e74265426591a9b1e61deef224f920e3b54fd01

--- glfw/cocoa_window.m
+++ glfw/cocoa_window.m
@@ -1689,8 +1689,8 @@ int _glfwPlatformWindowHovered(_GLFWwindow* window)
         return GLFW_FALSE;
     }
 
-    return NSPointInRect(point,
-        [window->ns.object convertRectToScreen:[window->ns.view bounds]]);
+    return NSMouseInRect(point,
+        [window->ns.object convertRectToScreen:[window->ns.view frame]], NO);
 }
 
 int _glfwPlatformFramebufferTransparent(_GLFWwindow* window)
