f6f55aaa9adfb1cd897749876e302e83eff37c38
Kovid Goyal <kovid@kovidgoyal.net>
2019-05-29 19:52:15 +0530

    NSGL: Remove enforcement of forward-compatible flag
    Upstream commit:
    https://github.com/glfw/glfw/commit/0b01d850ed00abf65ede29da46ad28ca5dbf8081

--- glfw/nsgl_context.m
+++ glfw/nsgl_context.m
@@ -132,12 +132,6 @@ GLFWbool _glfwCreateContextNSGL(_GLFWwindow* window,
             return GLFW_FALSE;
         }
 
-        if (!ctxconfig->forward || ctxconfig->profile != GLFW_OPENGL_CORE_PROFILE)
-        {
-            _glfwInputError(GLFW_VERSION_UNAVAILABLE,
-                            "NSGL: The targeted version of macOS only supports forward-compatible core profile contexts for OpenGL 3.2 and above");
-            return GLFW_FALSE;
-        }
     }
 
     // Context robustness modes (GL_KHR_robustness) are not yet supported by
