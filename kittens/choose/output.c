af3504e05c061d9d2eb9091d7d0f439b4a26d30c
Kovid Goyal <kovid@kovidgoyal.net>
2019-06-24 16:00:45 +0530

    Ensure that ensure_space() does not leave a NULL pointer

--- kittens/choose/output.c
+++ kittens/choose/output.c
@@ -25,7 +25,7 @@ static inline ssize_t ms_write(int fd, const void* buf, size_t count) { return _
 
 static inline bool
 ensure_space(GlobalData *global, size_t sz) {
-    if (global->output_sz < sz + global->output_pos) {
+    if (global->output_sz < sz + global->output_pos || !global->output) {
         size_t before = global->output_sz;
         global->output_sz += MAX(sz, (64 * 1024));
         global->output = realloc(global->output, sizeof(text_t) * global->output_sz);
