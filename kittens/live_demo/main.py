b8214c7006faa924c7d533a9308c33c7ce71558f
s1341 <github@shmarya.net>
2019-07-05 18:01:12 +0300

    [live kittens] live_demo now prompts for the search term

--- kittens/live_demo/main.py
+++ kittens/live_demo/main.py
@@ -2,27 +2,42 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2018, Kovid Goyal <kovid at kovidgoyal.net>
 
+from contextlib import suppress
 import fcntl
 import os
 import select
 import sys
-from ..tui.operations import styled
+from ..tui.operations import alternate_screen, styled
 
 
 def main(args):
-    text = ''
     if sys.stdin.isatty():
         if '--help' not in args and '-h' not in args:
             print('You must pass the text to be hinted on STDIN', file=sys.stderr)
             input('Press Enter to quit')
             return
 
+    global readline
+    import readline as rl
+    readline = rl
+    from kitty.shell import init_readline
+
+    init_readline(readline)
+
+    sys.stdin = open(os.ctermid())
+    term = ""
+    with alternate_screen():
+        with suppress(KeyboardInterrupt, EOFError):
+            term = input("Enter search term: ")
+    if term == "":
+        return
+
     fcntl.fcntl(sys.__stdin__.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
     try:
         while True:
             if select.select([sys.__stdin__], [], [], 0) == ([sys.__stdin__], [], []):
                 text = sys.__stdin__.read()
-                text = text.replace("hello", styled("hello", bold=True, bg="green", fg="black"))
+                text = text.replace(term, styled(term, bold=True, bg="green", fg="black"))
                 sys.stdout.write(text)
     except KeyboardInterrupt:
         pass
@@ -34,7 +49,7 @@ def handle_result(args, data, target_window_id, boss):
     pass
 
 
-handle_result.type_of_input = 'live'
+handle_result.type_of_input = 'live-history'
 
 
 if __name__ == '__main__':
