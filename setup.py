93af102becb96aafa5ee8ceb8f758530b7dddd3e
Kovid Goyal <kovid@kovidgoyal.net>
2019-07-07 04:52:24 +0530

    Clean link_commands.json

--- setup.py
+++ setup.py
@@ -887,7 +887,10 @@ def clean():
                 else:
                     os.unlink(x)
 
-    safe_remove('build', 'compile_commands.json', 'linux-package', 'kitty.app', 'asan-launcher', 'kitty-profile', 'kitty/launcher')
+    safe_remove(
+        'build', 'compile_commands.json', 'link_commands.json',
+        'linux-package', 'kitty.app', 'asan-launcher',
+        'kitty-profile', 'kitty/launcher')
     exclude = ('.git',)
     for root, dirs, files in os.walk('.', topdown=True):
         dirs[:] = [d for d in dirs if d not in exclude]
