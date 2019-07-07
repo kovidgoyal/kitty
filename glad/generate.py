f872f955b02b4c352939d6ebb507d80f73de5da9
David Herberth <github@dav1d.de>
2019-07-07 20:12:09 +0200

    Replace glad with glad2

--- glad/generate.py
+++ glad/generate.py
@@ -8,9 +8,11 @@ import shlex
 import shutil
 import subprocess
 
+
 cmdline = (
-    'glad --profile core --out-path {dest} --api gl=3.3 --generator {generator} --spec gl'
-    ' --extensions GL_ARB_texture_storage,GL_ARB_copy_image,GL_ARB_multisample,GL_ARB_robustness,GL_KHR_debug'
+    'glad --out-path {dest} --api gl:core=3.3 '
+    ' --extensions GL_ARB_texture_storage,GL_ARB_copy_image,GL_ARB_multisample,GL_ARB_robustness,GL_KHR_debug '
+    'c --header-only --debug'
 )
 
 
@@ -23,7 +25,7 @@ def regenerate():
     clean('out')
 
     subprocess.check_call(
-        shlex.split(cmdline.format(dest='out', generator='c-debug'))
+        shlex.split(cmdline.format(dest='out'))
     )
 
 
@@ -32,33 +34,12 @@ def strip_trailing_whitespace(c):
 
 
 def export():
-    c = open('out/src/glad.c', 'rb').read().decode('utf-8')
-    functions = []
+    with open('out/include/glad/gl.h', 'r', encoding='utf-8') as source:
+        data = source.read()
+        data = strip_trailing_whitespace(data)
 
-    def sub(m):
-        functions.append(m.group(2))
-        return m.group()
-
-    c = re.sub(r'^([A-Z0-9]+) glad_debug_([a-zA-Z0-9]+) = glad_debug_impl_\2;$', sub, c, flags=re.M)
-    switch = ['glad_debug_{0} = glad_{0}'.format(f) for f in functions]
-    c = c.replace('<glad/glad.h>', '"gl-wrapper.h"', 1)
-    c = '#pragma GCC diagnostic ignored "-Wpedantic"\n' + c
-    c += '''
-int
-init_glad(GLADloadproc load, int debug) {
-    int ret = gladLoadGLLoader(load);
-    if (ret && !debug) {
-        SUB;
-    }
-    return ret;
-}'''.replace('SUB', ';\n        '.join(switch), 1)
-    open('../kitty/gl-wrapper.c', 'w').write(strip_trailing_whitespace(c))
-    raw = open('out/include/glad/glad.h').read()
-    raw = raw.replace('<KHR/khrplatform.h>', '"khrplatform.h"')
-    raw += '\nint init_glad(GLADloadproc, int);\n'
-    open('../kitty/gl-wrapper.h', 'w').write(strip_trailing_whitespace(raw))
-    raw = open('out/include/KHR/khrplatform.h', 'rb').read().decode('utf-8')
-    open('../kitty/khrplatform.h', 'w').write(strip_trailing_whitespace(raw))
+        with open('../kitty/gl-wrapper.h', 'w', encoding='utf-8') as dest:
+            dest.write(data)
 
 
 if __name__ == '__main__':
