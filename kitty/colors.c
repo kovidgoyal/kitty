d9404c0362342a79e98162fa975382615027d287
Fabian <copy@copy.sh>
2019-09-11 19:55:08 +0200

    Fix color index 232 (should be #080808, not black)

--- kitty/colors.c
+++ kitty/colors.c
@@ -36,12 +36,12 @@ init_FG_BG_table(void) {
         // colors 16..232: the 6x6x6 color cube
         const uint8_t valuerange[6] = {0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff};
         uint8_t i, j=16;
-        for(i = 0; i < 217; i++, j++) {
+        for(i = 0; i < 216; i++, j++) {
             uint8_t r = valuerange[(i / 36) % 6], g = valuerange[(i / 6) % 6], b = valuerange[i % 6];
             FG_BG_256[j] = (r << 16) | (g << 8) | b;
         }
-        // colors 233..255: grayscale
-        for(i = 1; i < 24; i++, j++) {
+        // colors 232..255: grayscale
+        for(i = 0; i < 24; i++, j++) {
             uint8_t v = 8 + i * 10;
             FG_BG_256[j] = (v << 16) | (v << 8) | v;
         }
