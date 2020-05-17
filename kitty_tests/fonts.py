936dc74a271513ade5ae7c2bebd4a7dbed45ac42
Kovid Goyal <kovid@kovidgoyal.net>
2020-05-17 23:08:27 +0530

    Some more coalesce tests

--- kitty_tests/fonts.py
+++ kitty_tests/fonts.py
@@ -132,3 +132,9 @@ class Rendering(BaseTest):
         self.ae(coalesce_symbol_maps(q), {(1, 1): 'a', (2, 3): 'b', (4, 4): 'a'})
         q = {(2, 3): 'b', (1, 4): 'a'}
         self.ae(coalesce_symbol_maps(q), {(1, 4): 'a'})
+        q = {(1, 4): 'a', (2, 5): 'b'}
+        self.ae(coalesce_symbol_maps(q), {(1, 1): 'a', (2, 5): 'b'})
+        q = {(2, 5): 'b', (1, 4): 'a'}
+        self.ae(coalesce_symbol_maps(q), {(1, 4): 'a', (5, 5): 'b'})
+        q = {(1, 4): 'a', (2, 5): 'a'}
+        self.ae(coalesce_symbol_maps(q), {(1, 5): 'a'})
