269f294d1910fef9b5d83d5d2d86a83e619e5776
Luflosi <luflosi@luflosi.de>
2020-05-23 00:27:41 +0200

    Use CALayer instead of NSView for EGLNativeWindowType
    From upstream: https://github.com/glfw/glfw/commit/91eebe922de06a3ed4ba6622686c436c7941aa63.

--- glfw/cocoa_platform.h
+++ glfw/cocoa_platform.h
@@ -104,7 +104,7 @@ typedef VkResult (APIENTRY *PFN_vkCreateMetalSurfaceEXT)(VkInstance,const VkMeta
 #define _glfw_dlclose(handle) dlclose(handle)
 #define _glfw_dlsym(handle, name) dlsym(handle, name)
 
-#define _GLFW_EGL_NATIVE_WINDOW  ((EGLNativeWindowType) window->ns.view)
+#define _GLFW_EGL_NATIVE_WINDOW  ((EGLNativeWindowType) window->ns.layer)
 #define _GLFW_EGL_NATIVE_DISPLAY EGL_DEFAULT_DISPLAY
 
 #define _GLFW_PLATFORM_WINDOW_STATE         _GLFWwindowNS  ns
