cd76d109f528ab2103818a428f22947ff8839309
Kovid Goyal <kovid@kovidgoyal.net>
2020-08-21 07:50:35 +0530

    Fix a couple of documentation build warnings

--- kitty/config_data.py
+++ kitty/config_data.py
@@ -321,7 +321,9 @@ Note that this code is indexed by PostScript name, and not the font
 family. This allows you to define very precise feature settings; e.g. you can
 disable a feature in the italic font but not in the regular font.
 
-To get the PostScript name for a font, use :code:`kitty + list-fonts --psnames`::
+To get the PostScript name for a font, use :code:`kitty + list-fonts --psnames`:
+
+.. code-block:: sh
 
     $ kitty + list-fonts --psnames | grep Fira
     Fira Code
