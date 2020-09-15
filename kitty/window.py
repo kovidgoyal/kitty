58f78afc2e504dfd5fe55fe6f38370d8e9256a2b
Kovid Goyal <kovid@kovidgoyal.net>
2020-09-15 16:19:27 +0530

    Assume localhost is always local
    Required by the spec

--- kitty/window.py
+++ kitty/window.py
@@ -508,7 +508,7 @@ class Window:
                     except Exception:
                         hostname = ''
                     remote_hostname = purl.netloc.partition(':')[0]
-                    if remote_hostname and remote_hostname != hostname:
+                    if remote_hostname and remote_hostname != hostname and remote_hostname != 'localhost':
                         self.handle_remote_file(purl.netloc, unquote(purl.path))
                         return
             if self.opts.allow_hyperlinks & 0b10:
