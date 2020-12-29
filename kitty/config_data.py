9c6e3a1b203fc20c65db4a6ead24b4938ce49183
Russell Davies <russell@zeroflux.net>
2020-12-29 18:52:30 +0100

    macOS: Fix change font size keyboard shortcut
    Fixes #2839.

--- kitty/config_data.py
+++ kitty/config_data.py
@@ -1475,8 +1475,7 @@ k('next_layout', 'kitty_mod+l', 'next_layout', _('Next layout'))
 g('shortcuts.fonts')  # {{{
 k('increase_font_size', 'kitty_mod+equal', 'change_font_size all +2.0', _('Increase font size'))
 if is_macos:
-    k('increase_font_size', 'cmd+plus', 'change_font_size all +2.0', _('Increase font size'), add_to_docs=False)
-    k('increase_font_size', 'cmd+shift+equal', 'change_font_size all +2.0', _('Increase font size'), add_to_docs=False)
+    k('increase_font_size', 'cmd+equal', 'change_font_size all +2.0', _('Increase font size'), add_to_docs=False)
 k('decrease_font_size', 'kitty_mod+minus', 'change_font_size all -2.0', _('Decrease font size'))
 if is_macos:
     k('decrease_font_size', 'cmd+minus', 'change_font_size all -2.0', _('Decrease font size'), add_to_docs=False)
