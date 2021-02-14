6b9c2802148c10769b200ff29814852d76e36fbe
Kovid Goyal <kovid@kovidgoyal.net>
2021-02-15 05:27:32 +0530

    Add a test for encoding of : as a legacy ascii key

--- kitty_tests/keys.py
+++ kitty_tests/keys.py
@@ -392,6 +392,7 @@ class TestKeys(BaseTest):
         # }}}
 
         ae(enc(key=ord(':'), shifted_key=ord('/'), mods=shift | alt), '\x1b/')
+        ae(enc(key=ord(':'), mods=alt), '\x1b:')
         ae(enc(key=ord(' ')), ' ')
         ae(enc(key=ord(' '), mods=ctrl), '\0')
         ae(enc(key=ord(' '), mods=alt), '\x1b ')
