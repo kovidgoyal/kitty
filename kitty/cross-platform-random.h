9ad5ef8b2d995299b0170d204a28d58b4f0f93ae
Kovid Goyal <kovid@kovidgoyal.net>
2021-02-16 12:39:18 +0530

    Fix compilation on ancient Linux distros without sys/random.h

--- kitty/cross-platform-random.h
+++ kitty/cross-platform-random.h
@@ -9,8 +9,9 @@
 #include <stdbool.h>
 
 #if __linux__
-#include <sys/random.h>
 #include <errno.h>
+#if __has_include(<sys/random.h>)
+#include <sys/random.h>
 
 static inline bool
 secure_random_bytes(void *buf, size_t nbytes) {
@@ -26,6 +27,25 @@ secure_random_bytes(void *buf, size_t nbytes) {
         left -= n; p += n;
     }
 }
+#else
+#include "safe-wrappers.h"
+static inline bool
+secure_random_bytes(void *buf, size_t nbytes) {
+    int fd = safe_open("/dev/urandom", O_RDONLY, 0);
+    if (fd < 0) return false;
+    size_t bytes_read = 0;
+    while (bytes_read < nbytes) {
+        ssize_t n = read(fd, (uint8_t*)buf + bytes_read, nbytes - bytes_read);
+        if (n < 0) {
+            if (errno == EINTR) continue;
+            break;
+        }
+        bytes_read += n;
+    }
+    safe_close(fd, __FILE__, __LINE__);
+    return bytes_read == nbytes;
+}
+#endif
 #else
 static inline bool
 secure_random_bytes(void *buf, size_t nbytes) {
