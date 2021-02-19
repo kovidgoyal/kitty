45d89cfe552f5d2d42782cce4d4992e317521c9f
Kovid Goyal <kovid@kovidgoyal.net>
2021-02-19 15:08:47 +0530

    Install importlib.resources on python 3.6

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -43,7 +43,10 @@ def install_deps():
         if is_macos:
             # needed for zlib for pillow, should not be needed after pillow 8.0
             os.environ['PKG_CONFIG_PATH'] = '/usr/local/opt/zlib/lib/pkgconfig'
-        run('pip3 install Pillow pygments')
+        cmd = 'pip3 install Pillow pygments'
+        if sys.version[:2] < (3, 7):
+            cmd += ' importlib-resources'
+        run(cmd)
 
 
 def build_kitty():
