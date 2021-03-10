48d2aa14c83fda3f8109fa0006a96c09de2cd38a
Kovid Goyal <kovid@kovidgoyal.net>
2021-03-10 08:20:35 +0530

    Install libx11-xcb-dev on CI
    Looks like it is no longer pre-installed

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -35,7 +35,7 @@ def install_deps():
     else:
         run('sudo apt-get update')
         run('sudo apt-get install -y libgl1-mesa-dev libxi-dev libxrandr-dev libxinerama-dev'
-            ' libxcursor-dev libxcb-xkb-dev libdbus-1-dev libxkbcommon-dev libharfbuzz-dev'
+            ' libxcursor-dev libxcb-xkb-dev libdbus-1-dev libxkbcommon-dev libharfbuzz-dev libx11-xcb-dev'
             ' libpng-dev liblcms2-dev libfontconfig-dev libxkbcommon-x11-dev libcanberra-dev uuid-dev')
     if is_bundle:
         install_bundle()
