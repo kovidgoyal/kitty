bbb40f2a2181caba87abd5b2eb5a8e5c6f8d46c5
Kovid Goyal <kovid@kovidgoyal.net>
2021-04-01 12:20:53 +0530

    Use a little less contrast for CSD title

--- glfw/wl_client_side_decorations.c
+++ glfw/wl_client_side_decorations.c
@@ -54,7 +54,7 @@ render_title_bar(_GLFWwindow *window, bool to_front_buffer) {
     uint32_t bg_color = is_focused ? active_bg_color : passive_bg_color;
     uint8_t *output = to_front_buffer ? decs.top.buffer.data.front : decs.top.buffer.data.back;
     if (window->wl.title && window->wl.title[0] && _glfw.callbacks.draw_text) {
-        uint32_t fg_color = is_focused ? 0xff222222 : 0xff888888;
+        uint32_t fg_color = is_focused ? 0xff444444 : 0xff888888;
         if (_glfw.callbacks.draw_text((GLFWwindow*)window, window->wl.title, fg_color, bg_color, output, decs.top.buffer.width, decs.top.buffer.height, 10, 0)) return;
     }
     for (uint32_t *px = (uint32_t*)output, *end = (uint32_t*)(output + decs.top.buffer.size_in_bytes); px < end; px++) {
