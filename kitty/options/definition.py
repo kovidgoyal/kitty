3e598a17cfbaa692edb226e1910e4eea70dd4020
Kovid Goyal <kovid@kovidgoyal.net>
2021-05-30 21:49:01 +0530

    Add headers to options def files

--- kitty/options/definition.py
+++ kitty/options/definition.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2021, Kovid Goyal <kovid at kovidgoyal.net>
+
+# After editing this file run ./gen-config.py to apply the changes
+
 from kitty.conf.types import Action, Definition
 
 
