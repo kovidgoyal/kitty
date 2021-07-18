fe18a5de927b3d3478a706f55de1c4b263f6c4c5
Kovid Goyal <kovid@kovidgoyal.net>
2021-07-18 22:19:47 +0530

    Use a shorter title

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,7 @@ rst_prolog = '''
 # a list of builtin themes.
 #
 html_theme = 'furo'
+html_title = 'kitty'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
