0c71b92162156268b4ae0a4cc891093814fe9329
swirl <roachh@protonmail.com>
2021-07-21 12:09:44 -0400

    default update_check_interval to 0

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ class Options(argparse.Namespace):
     extra_logging: List[str] = []
     extra_include_dirs: List[str] = []
     link_time_optimization: bool = 'KITTY_NO_LTO' not in os.environ
-    update_check_interval: float = 24
+    update_check_interval: float = 0
     egl_library: Optional[str] = os.getenv('KITTY_EGL_LIBRARY')
     startup_notification_library: Optional[str] = os.getenv('KITTY_STARTUP_NOTIFICATION_LIBRARY')
     canberra_library: Optional[str] = os.getenv('KITTY_CANBERRA_LIBRARY')
@@ -1112,10 +1112,10 @@ def package(args: Options, bundle_type: str) -> None:
     shutil.copytree('kittens', os.path.join(libdir, 'kittens'), ignore=src_ignore)
     if for_freeze:
         shutil.copytree('kitty_tests', os.path.join(libdir, 'kitty_tests'))
-    if args.update_check_interval != 24.0:
+    if args.update_check_interval != 0.0:
         with open(os.path.join(libdir, 'kitty/options/types.py'), 'r+', encoding='utf-8') as f:
             raw = f.read()
-            nraw = raw.replace('update_check_interval: float = 24.0', f'update_check_interval: float = {args.update_check_interval!r}', 1)
+            nraw = raw.replace('update_check_interval: float = 0.0', f'update_check_interval: float = {args.update_check_interval!r}', 1)
             if nraw == raw:
                 raise SystemExit('Failed to change the value of update_check_interval')
             f.seek(0), f.truncate(), f.write(nraw)
