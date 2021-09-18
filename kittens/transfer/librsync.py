901a075a38e02d28b222385b68a9c6551ca75642
Kovid Goyal <kovid@kovidgoyal.net>
2021-09-18 11:51:54 +0530

    ...

--- kittens/transfer/librsync.py
+++ kittens/transfer/librsync.py
@@ -5,14 +5,14 @@
 import os
 from typing import Iterator
 
-from .rsync import IO_BUFFER_SIZE, begin_signature, iter_job
+from .rsync import IO_BUFFER_SIZE, begin_create_signature, iter_job
 
 
 def signature_of_file(path: str) -> Iterator[bytes]:
     with open(path, 'rb') as f:
         f.seek(0, os.SEEK_END)
         fsz = f.tell()
-        job = begin_signature(fsz)
+        job = begin_create_signature(fsz)
         f.seek(0)
         finished = False
         while not finished:
