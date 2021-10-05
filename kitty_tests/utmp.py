57152a8e29d951c2447a83f5bcf5c43f3163d106
Kovid Goyal <kovid@kovidgoyal.net>
2021-10-05 11:41:18 +0530

    Fix utmp test

--- kitty_tests/utmp.py
+++ kitty_tests/utmp.py
@@ -1,13 +1,14 @@
-from sys import platform
+import subprocess
+from kitty.fast_data_types import num_users
+from . import BaseTest
 
-if platform in ('linux', 'linux2'):
-    import subprocess
-    import re
-    from kitty.fast_data_types import num_users
-    from . import BaseTest
 
-    class UTMPTest(BaseTest):
-        def test_num_users(self):
-            # who -q is the control
-            expected = subprocess.run(['who'], capture_output=True).stdout.decode('utf-8').count('\n')
-            self.ae(num_users(), expected)
+class UTMPTest(BaseTest):
+
+    def test_num_users(self):
+        # who is the control
+        try:
+            expected = subprocess.check_output(['who']).decode('utf-8').count('\n')
+        except FileNotFoundError:
+            self.skipTest('No who executable cannot verify num_users')
+        self.ae(num_users(), expected)
