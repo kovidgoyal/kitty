5064b5c2b119be60b5465597d73d243e55caa150
Kovid Goyal <kovid@kovidgoyal.net>
2022-02-23 22:19:41 +0530

    Apparently posh on CI is utterly broken

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -37,7 +37,7 @@ def install_deps():
         run('sudo apt-get install -y libgl1-mesa-dev libxi-dev libxrandr-dev libxinerama-dev ca-certificates'
             ' libxcursor-dev libxcb-xkb-dev libdbus-1-dev libxkbcommon-dev libharfbuzz-dev libx11-xcb-dev zsh'
             ' libpng-dev liblcms2-dev libfontconfig-dev libxkbcommon-x11-dev libcanberra-dev librsync-dev uuid-dev'
-            ' zsh bash dash posh')
+            ' zsh bash dash')
         # for some reason these directories are world writable which causes zsh
         # compinit to break
         run('sudo chmod -R og-w /usr/share/zsh')
