1096cbe23618977a0a1b008928f50bbea0aaeff3
Kovid Goyal <kovid@kovidgoyal.net>
2022-03-01 07:46:45 +0530

    Use an anonymous struct

--- glfw/wl_platform.h
+++ glfw/wl_platform.h
@@ -128,11 +128,6 @@ typedef enum WaylandWindowState {
 static const WaylandWindowState TOPLEVEL_STATE_DOCKED = TOPLEVEL_STATE_MAXIMIZED | TOPLEVEL_STATE_FULLSCREEN | TOPLEVEL_STATE_TILED_TOP | TOPLEVEL_STATE_TILED_LEFT | TOPLEVEL_STATE_TILED_RIGHT | TOPLEVEL_STATE_TILED_BOTTOM;
 
 
-typedef struct _GLFWwindowWaylandState {
-    int                         width, height;
-    uint32_t toplevel_states;
-} _GLFWwindowWaylandState;
-
 // Wayland-specific per-window data
 //
 typedef struct _GLFWwindowWayland
@@ -221,7 +216,10 @@ typedef struct _GLFWwindowWayland
         unsigned int x, y;
     } axis_discrete_count;
 
-    _GLFWwindowWaylandState current, pending;
+    struct {
+        int width, height;
+        uint32_t toplevel_states;
+    } current, pending;
 } _GLFWwindowWayland;
 
 typedef enum _GLFWWaylandOfferType
