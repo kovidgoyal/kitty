e7b216ba6834b169b8bb257632e6eb02266ec54d
Kovid Goyal <kovid@kovidgoyal.net>
2022-04-07 16:24:21 +0530

    DRYer

--- kitty/mouse.c
+++ kitty/mouse.c
@@ -67,13 +67,11 @@ static char mouse_event_buf[64];
 
 static int
 encode_mouse_event_impl(const MousePosition *mpos, int mouse_tracking_protocol, int button, MouseAction action, int mods) {
-    unsigned int cb = 0;
+    unsigned int cb = encode_button(button);
     if (action == MOVE) {
-        cb = encode_button(button);
         if (cb == UINT_MAX) cb = 3;
         cb += 32;
     } else {
-        cb = encode_button(button);
         if (cb == UINT_MAX) return 0;
     }
     if (action == DRAG || action == MOVE) cb |= MOTION_INDICATOR;
