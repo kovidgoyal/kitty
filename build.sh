f5fc0dcc640e990cd132e07a63928c654ddcd0a4
nguyenvukhang <brew4k@gmail.com>
2022-06-25 10:14:00 +0800

    remove custom build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LDFLAGS=-L/opt/homebrew/lib \
-    python3 setup.py kitty.app \
-    --extra-include-dirs /opt/homebrew/Cellar/librsync/2.3.2/include
-rm -rf /Applications/kitty.app
-cp -r kitty.app /Applications/kitty.app
