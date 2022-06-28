297dea881da52348746fc423a25c19567084ae2c
pagedown <pg.dn@outlook.com>
2022-06-28 11:35:11 +0800

    Use the same fish completion function for commands

--- shell-integration/fish/vendor_completions.d/clone-in-kitty.fish
+++ shell-integration/fish/vendor_completions.d/clone-in-kitty.fish
@@ -1,7 +1,6 @@
 function __ksi_completions
     set --local ct (commandline --current-token)
     set --local tokens (commandline --tokenize --cut-at-cursor --current-process)
-    set tokens[1] "clone-in-kitty"
     printf "%s\n" $tokens $ct | command kitty +complete fish2
 end
 
