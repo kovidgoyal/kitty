ab2db70421277c1436b8f0dcfe41c285234f7da3
Kovid Goyal <kovid@kovidgoyal.net>
2022-08-14 18:47:01 +0530

    Show help when command is specified

--- tools/cmd/at/main.go
+++ tools/cmd/at/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 
 	"kitty/tools/cli"
@@ -11,6 +12,7 @@ import (
 )
 
 var encrypt_cmd = crypto.Encrypt_cmd
+var exe_name = "kitty-at"
 
 func main() {
 	var root = cli.CreateCommand(&cobra.Command{
@@ -18,10 +20,10 @@ func main() {
 		Short: "Control kitty remotely",
 		Long:  "Control kitty by sending it commands. Set the allow_remote_control option in kitty.conf or use a password, for this to work.",
 		Run: func(cmd *cobra.Command, args []string) {
-			use_password, _ := cmd.Flags().GetString("use-password")
-			fmt.Println("In global run, use-password:", use_password)
+            cmd.Usage()
+            fmt.Fprintln(os.Stderr, color.RedString("\nNo command specified for " + exe_name))
 		},
-	}, "kitty-at")
+	}, exe_name)
 	cli.PersistentChoices(root, "use-password", "If no password is available, kitty will usually just send the remote control command without a password. This option can be used to force it to always or never use the supplied password.", "if-available", "always", "never")
 	root.Annotations["options_title"] = "Global options"
 	cli.Init(root)
