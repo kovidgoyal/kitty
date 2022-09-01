67115530b4acadbeaab406a9e8c03fc1fca52fa2
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-01 12:14:03 +0530

    Make the loop API a thin wrapper around internal methods

--- tools/tui/loop/timers.go
+++ tools/tui/loop/timers.go
@@ -3,9 +3,35 @@
 package loop
 
 import (
+	"fmt"
 	"sort"
 	"time"
 )
+
+func (self *Loop) add_timer(interval time.Duration, repeats bool, callback TimerCallback) (IdType, error) {
+	if self.timers == nil {
+		return 0, fmt.Errorf("Cannot add timers before starting the run loop, add them in OnInitialize instead")
+	}
+	self.timer_id_counter++
+	t := timer{interval: interval, repeats: repeats, callback: callback, id: self.timer_id_counter}
+	t.update_deadline(time.Now())
+	self.timers = append(self.timers, &t)
+	self.sort_timers()
+	return t.id, nil
+}
+
+func (self *Loop) remove_timer(id IdType) bool {
+	if self.timers == nil {
+		return false
+	}
+	for i := 0; i < len(self.timers); i++ {
+		if self.timers[i].id == id {
+			self.timers = append(self.timers[:i], self.timers[i+1:]...)
+			return true
+		}
+	}
+	return false
+}
 
 func (self *Loop) dispatch_timers(now time.Time) error {
 	updated := false
