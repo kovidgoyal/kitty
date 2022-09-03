07c16f95f135912efabb5c52c9fd58dd281dc797
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-03 07:01:27 +0530

    Dont drop trailing escape codes when stripping trailing whitespace

--- tools/utils/style/indent-and-wrap_test.go
+++ tools/utils/style/indent-and-wrap_test.go
@@ -21,6 +21,6 @@ func TestFormatWithIndent(t *testing.T) {
 	tx("testing\n\ntwo", "testing\n\n__two")
 	tx("testing\n \ntwo", "testing\n\n__two")
 
-	tx("123456 \x1b[31m789a", "123456\n\x1b[39m__\x1b[31m789a")
+	tx("123456 \x1b[31m789a", "123456\x1b[31m\n\x1b[39m__\x1b[31m789a")
 	tx("12 \x1b[31m789 abcd", "12 \x1b[31m789\n\x1b[39m__\x1b[31mabcd")
 }
