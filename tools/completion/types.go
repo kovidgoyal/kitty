63287e4115c94ad9a09f45e9c991204aff332686
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-07 10:55:07 +0530

    More work on completions

--- tools/completion/types.go
+++ tools/completion/types.go
@@ -23,7 +23,7 @@ type Completions struct {
 	current_cmd *Command
 }
 
-type completion_func func(completions *Completions, partial_word string)
+type completion_func func(completions *Completions, partial_word string, arg_num int)
 
 type Option struct {
 	Name               string
@@ -33,29 +33,58 @@ type Option struct {
 	Completion_for_arg completion_func
 }
 
+type CommandGroup struct {
+	Title    string
+	Commands []*Command
+}
+
 type Command struct {
 	Name        string
 	Description string
 
-	// List of options for this command
 	Options []*Option
-
-	// List of subcommands
-	Subcommands []*Command
-	// Optional title used as a header when displaying the list of matching sub-commands for a completion
-	Subcommands_title string
+	Groups  []*CommandGroup
 
 	Completion_for_arg     completion_func
 	Stop_processing_at_arg int
 }
 
-var Root = Command{Options: make([]*Option, 0), Subcommands: make([]*Command, 0, 32)}
+func (self *Command) add_command(name string) *Command {
+	ans := Command{Name: name}
+	ans.Options = make([]*Option, 0, 8)
+	ans.Groups = make([]*CommandGroup, 0, 2)
+	return &ans
+}
 
-func GetCompletions(argv []string) *Completions {
+func (self *Command) find_subcommand(is_ok func(cmd *Command) bool) *Command {
+	for _, g := range self.Groups {
+		for _, q := range g.Commands {
+			if is_ok(q) {
+				return q
+			}
+		}
+	}
+	return nil
+}
+
+func (self *Command) find_subcommand_with_name(name string) *Command {
+	return self.find_subcommand(func(cmd *Command) bool { return cmd.Name == name })
+}
+
+func (self *Command) has_subcommands() bool {
+	for _, g := range self.Groups {
+		if len(g.Commands) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (self *Command) GetCompletions(argv []string) *Completions {
 	ans := Completions{Groups: make([]*MatchGroup, 0, 4)}
 	if len(argv) > 0 {
 		exe := argv[0]
-		cmd := Root.find_subcommand(exe)
+		cmd := self.find_subcommand_with_name(exe)
 		if cmd != nil {
 			cmd.parse_args(argv[1:], &ans)
 		}
