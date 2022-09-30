8f5302a65023b70e816aa74e9690b47e8fd4dc51
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-30 13:39:15 +0530

    Hook up kitty-tool completions with shell integration

---
+++ shell-integration/fish/vendor_completions.d/kitty-tool.fish
@@ -0,0 +1,7 @@
+function __ksi_completions
+    set --local ct (commandline --current-token)
+    set --local tokens (commandline --tokenize --cut-at-cursor --current-process)
+    printf "%s\n" $tokens $ct | command kitty-tool __complete__ fish
+end
+
+complete -f -c kitty-tool -a "(__ksi_completions)"
