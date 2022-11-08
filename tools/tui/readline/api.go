31dcb13836da514bbe0bdfdf5e775fad2f0b634f
Kovid Goyal <kovid@kovidgoyal.net>
2022-11-08 10:43:57 +0530

    cursor_y should be zero when resetting

--- tools/tui/readline/api.go
+++ tools/tui/readline/api.go
@@ -235,6 +235,7 @@ func (self *Readline) ResetText() {
 	self.last_action = ActionNil
 	self.keyboard_state = KeyboardState{}
 	self.history_search = nil
+	self.cursor_y = 0
 }
 
 func (self *Readline) ChangeLoopAndResetText(lp *loop.Loop) {
