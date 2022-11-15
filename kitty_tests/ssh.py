16775c55393547be14d3d97a8a4fa4dc4b98944c
Kovid Goyal <kovid@kovidgoyal.net>
2022-11-15 13:26:48 +0530

    ssh kitten: Add a kitty-tool wrapper script to auto-download and use kitty-tool
    Needs testing.

--- kitty_tests/ssh.py
+++ kitty_tests/ssh.py
@@ -144,7 +144,8 @@ copy --exclude */w.* d1
                 contents.discard(f'{tname}/78/xterm-kitty')
                 self.ae(contents, {
                     'g.1', 'g.2', f'{tname}/kitty.terminfo', 'simple-file', 'd1/d2/x', 'd1/y', 'a/sfa', 's1', 's2',
-                    '.local/share/kitty-ssh-kitten/kitty/version', '.local/share/kitty-ssh-kitten/kitty/bin/kitty'
+                    '.local/share/kitty-ssh-kitten/kitty/version', '.local/share/kitty-ssh-kitten/kitty/bin/kitty',
+                    '.local/share/kitty-ssh-kitten/kitty/bin/kitty-tool'
                 })
                 self.ae(len(glob.glob(f'{remote_home}/{tname}/*/xterm-kitty')), 2)
 
