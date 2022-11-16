5e880b92f58d5a2502446690eb9e51cec4768d56
Kovid Goyal <kovid@kovidgoyal.net>
2022-11-16 20:32:42 +0530

    Ensure active hyperlink is cleared when truncating output for completion descriptions

--- tools/cli/zsh.go
+++ tools/cli/zsh.go
@@ -67,7 +67,7 @@ func (self *Match) FormatForCompletionList(max_word_len int, f *markup.Context, 
 		word += strings.Repeat(" ", max_word_len-word_len)
 	}
 	if wcswidth.Stringwidth(desc) > max_desc_len {
-		desc = wcswidth.TruncateToVisualLength(desc, max_desc_len-2) + "\x1b[m…"
+		desc = wcswidth.TruncateToVisualLength(desc, max_desc_len-2) + "\x1b[m\x1b]8;;\x1b\\…"
 	}
 	if multiline {
 		return word + "\n" + strings.Repeat(" ", max_word_len+2) + desc
