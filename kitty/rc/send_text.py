aa9080d375c0c792c4e27b851d65de0537be2451
Kovid Goyal <kovid@kovidgoyal.net>
2022-11-26 15:38:22 +0530

    Add a note about using single quotes when passing arguments with backslash escapes to send-text
    One could of course also use double quotes and escape the backslash or
    just escape the backslash but I dont think we need to explain shell
    quoting fully here.

--- kitty/rc/send_text.py
+++ kitty/rc/send_text.py
@@ -75,8 +75,9 @@ class SendText(RemoteCommand):
     desc = (
         'Send arbitrary text to specified windows. The text follows Python'
         ' escaping rules. So you can use :link:`escapes <https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html>`'
-        ' like :code:`\\\\x1b` to send control codes'
-        ' and :code:`\\\\u21fa` to send Unicode characters. If you use the :option:`kitty @ send-text --match` option'
+        " like :code:`'\\\\x1b'` to send control codes"
+        " and :code:`'\\\\u21fa'` to send Unicode characters. Remember to use single-quotes otherwise"
+        ' the backslash is interpreted as a shell escape character. If you use the :option:`kitty @ send-text --match` option'
         ' the text will be sent to all matched windows. By default, text is sent to'
         ' only the currently active window.'
     )
