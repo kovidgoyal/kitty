cc5107d0dbb329910c0a59030a7ff2a431fedf47
Kovid Goyal <kovid@kovidgoyal.net>
2023-02-09 21:33:18 +0530

    Convenient way to load/save JSON data

---
+++ tools/utils/cached_values.go
@@ -0,0 +1,40 @@
+// License: GPLv3 Copyright: 2023, Kovid Goyal, <kovid at kovidgoyal.net>
+
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+var _ = fmt.Print
+
+type CachedValues[T any] struct {
+	Name string
+	Opts T
+}
+
+func (self *CachedValues[T]) Path() string {
+	return filepath.Join(CacheDir(), self.Name+".json")
+}
+
+func (self *CachedValues[T]) Load() T {
+	raw, err := os.ReadFile(self.Path())
+	if err == nil {
+		json.Unmarshal(raw, self.Opts)
+	}
+	return self.Opts
+}
+
+func (self *CachedValues[T]) Save() {
+	raw, err := json.Marshal(self.Opts)
+	if err == nil {
+		os.WriteFile(self.Path(), raw, 0600)
+	}
+}
+
+func NewCachedValues[T any](name string, initial_val T) *CachedValues[T] {
+	return &CachedValues[T]{Name: name, Opts: initial_val}
+}
