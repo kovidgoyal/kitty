373c05943fb4baf4874fbab72445401fbf6995f3
Kovid Goyal <kovid@kovidgoyal.net>
2023-04-08 13:35:38 +0530

    Allow specifying full layout specifications with options for goto_layout
    Fixes #6163

--- kitty/rc/goto_layout.py
+++ kitty/rc/goto_layout.py
@@ -25,12 +25,14 @@ class GotoLayout(RemoteCommand):
     desc = (
         'Set the window layout in the specified tabs (or the active tab if not specified).'
         ' You can use special match value :code:`all` to set the layout in all tabs.'
+        ' In case there are multiple layouts with the same name but different options,'
+        ' specify the full layout definition or a unique prefix of the full definition.'
     )
     options_spec = MATCH_TAB_OPTION
     args = RemoteCommand.Args(
         spec='LAYOUT_NAME', count=1, json_field='layout',
         completion=RemoteCommand.CompletionSpec.from_string('type:keyword group:"Layout" kwds:' + ','.join(layout_names())),
-        args_choices=layout_names)
+        )
 
     def message_to_kitty(self, global_opts: RCOptions, opts: 'CLIOptions', args: ArgsType) -> PayloadType:
         if len(args) != 1:
@@ -44,7 +46,7 @@ class GotoLayout(RemoteCommand):
                 try:
                     tab.goto_layout(payload_get('layout'), raise_exception=True)
                 except ValueError:
-                    raise UnknownLayout('The layout {} is unknown or disabled'.format(payload_get('layout')))
+                    raise UnknownLayout('The layout {} is unknown or disabled or the name is ambiguous'.format(payload_get('layout')))
         return None
 
 
