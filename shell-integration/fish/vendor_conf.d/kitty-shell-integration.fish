4c56e768401f920bb3ce129e1883810b7e6323f7
Kovid Goyal <kovid@kovidgoyal.net>
2023-11-11 20:00:46 +0530

    Fix #6812

--- shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish
+++ shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish
@@ -111,7 +111,7 @@ function __ksi_schedule --on-event fish_prompt -d "Setup kitty integration after
     # Note that neither alias nor function is recursive in fish so if the user defines an alias/function
     # for sudo it will be clobbered by us, so only install this if sudo is not already function
     if not contains "no-sudo" $_ksi
-        and test -n "$TERMINFO" -a "file" = (type -t sudo)
+        and test -n "$TERMINFO" -a "file" = (type -t sudo 2> /dev/null || echo "x")
         and not test -r "/usr/share/terminfo/x/xterm-kitty" -o -r "/usr/share/terminfo/78/xterm-kitty" 
         # Ensure terminfo is available in sudo
         function sudo
