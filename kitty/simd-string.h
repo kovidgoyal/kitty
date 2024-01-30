d217335cff678d6d1e8a1ea11c67365b2408bae8
Kovid Goyal <kovid@kovidgoyal.net>
2024-01-30 19:52:06 +0530

    remove declaration without implementation

--- kitty/simd-string.h
+++ kitty/simd-string.h
@@ -25,15 +25,10 @@ bool utf8_decode_to_esc(UTF8Decoder *d, const uint8_t *src, size_t src_sz);
 // Pass a PyModule PyObject* as the argument. Must be called once at application startup
 bool init_simd(void* module);
 
-// Requires 7 bytes to the left of haystack to be readable. Returns pointer to
+// Requires 31 bytes to the right of haystack to be readable. Returns pointer to
 // first position in haystack that contains either of the two chars or NULL if
 // not found.
 const uint8_t* find_either_of_two_bytes(const uint8_t *haystack, const size_t sz, const uint8_t a, const uint8_t b);
-
-// Requires 7 bytes to the left of haystack to be readable. Returns pointer to
-// first position in haystack that contains a char that is not in [a, b].
-// a must be <= b
-const uint8_t* find_byte_not_in_range(const uint8_t *haystack, const size_t sz, const uint8_t a1, const uint8_t b);
 
 // SIMD implementations, internal use
 bool utf8_decode_to_esc_128(UTF8Decoder *d, const uint8_t *src, size_t src_sz);
