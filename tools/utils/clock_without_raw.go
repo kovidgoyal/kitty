3d98b3307655151b368c92afc2f0e5b578e6657f
Kovid Goyal <kovid@kovidgoyal.net>
2024-04-09 08:39:42 +0530

    ...

--- tools/utils/clock_without_raw.go
+++ tools/utils/clock_without_raw.go
@@ -4,15 +4,8 @@ package utils
 
 import (
 	"time"
-
-	"golang.org/x/sys/unix"
 )
 
 func MonotonicRaw() (time.Time, error) {
-	ts := unix.Timespec{}
-	if err := unix.ClockGettime(unix.CLOCK_MONOTONIC, &ts); err != nil {
-		return time.Time{}, err
-	}
-	s, ns := ts.Unix()
-	return time.Unix(s, ns), nil
+	return time.Now(), nil
 }
