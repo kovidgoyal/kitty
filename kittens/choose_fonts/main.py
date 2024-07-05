d8dc237af7558e786a0df4b43c2561a64b64e764
Kovid Goyal <kovid@kovidgoyal.net>
2024-07-05 12:37:54 +0530

    ...

--- kittens/choose_fonts/main.py
+++ kittens/choose_fonts/main.py
@@ -1,6 +1,8 @@
 import os
 
+from kitty.constants import kitten_exe
+
 
 def main(args: 'list[str]') -> None:
     # allow running this kitten via map key kitten choose-fonts
-    os.execlp('kitten', 'kitten', *args)
+    os.execlp(kitten_exe(), 'kitten', *args)
