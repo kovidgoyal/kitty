d9b1c8c04f3020a28da5de3128fa3e3c54f24a44
Kovid Goyal <kovid@kovidgoyal.net>
2024-09-02 11:38:31 +0530

    Fix a harmless error being printed to stderr if shell integration sends an empty cmdline
    Fixes #7813

--- kitty/window.py
+++ kitty/window.py
@@ -213,8 +213,9 @@ def compile_match_query(exp: str, is_simple: bool = True) -> MatchPatternType:
 def decode_cmdline(x: str) -> str:
     ctype, sep, val = x.partition('=')
     if ctype == 'cmdline':
-        return next(shlex_split(val, True))
-    if ctype == 'cmdline_url':
+        with suppress(StopIteration):
+            return next(shlex_split(val, True))
+    elif ctype == 'cmdline_url':
         from urllib.parse import unquote
         return unquote(val)
     return ''
