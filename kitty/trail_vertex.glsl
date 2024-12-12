afd0b1f913825178566c0b98970e7cf563507587
Kovid Goyal <kovid@kovidgoyal.net>
2024-12-12 08:51:27 +0530

    DRYer

--- kitty/trail_vertex.glsl
+++ kitty/trail_vertex.glsl
@@ -4,6 +4,7 @@ uniform vec4 y_coords;
 out vec2 frag_pos;
 
 void main() {
-    frag_pos = vec2(x_coords[gl_VertexID], y_coords[gl_VertexID]);
-    gl_Position = vec4(x_coords[gl_VertexID], y_coords[gl_VertexID], 1.0, 1.0);
+    vec2 pos = vec2(x_coords[gl_VertexID], y_coords[gl_VertexID]);
+    gl_Position = vec4(pos, 1.0, 1.0);
+    frag_pos = pos;
 }
