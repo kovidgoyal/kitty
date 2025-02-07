kovidgoyal/kitty

commit 2bfeb136289edc9233b87099d39292f25615e787

XTGETTCAP: Also respond for boolean capabilities from terminfo

kitty/terminfo.py
@@ -521,8 +521,9 @@ def get_capabilities(query_string: str, opts: 'Options') -> Generator[str, None,
     from .fast_data_types import ERROR_PREFIX
 
     def result(encoded_query_name: str, x: Optional[str] = None) -> str:
-        if x is None:
-            return f'0+r{encoded_query_name}'
+        if not x:
+            valid = 0 if x is None else 1
+            return f'{valid}+r{encoded_query_name}'
         return f'1+r{encoded_query_name}={hexlify(str(x).encode("utf-8")).decode("ascii")}'
 
     for encoded_query_name in query_string.split(';'):
@@ -540,6 +541,9 @@ def get_capabilities(query_string: str, opts: 'Options') -> Generator[str, None,
             else:
                 yield result(encoded_query_name, rval)
         else:
+            if name in bool_capabilities:
+                yield result(encoded_query_name, '')
+                continue
             try:
                 val = queryable_capabilities[name]
             except KeyError:

commit 8288e3ec685274cfec6053e92d9930e4b833aa17

XTGETTCAP: Fix DCS response for empty capabilities
fixes: commit 2bfeb13628

kitty/terminfo.py
@@ -521,10 +521,11 @@ def key_as_bytes(name: str) -> bytes:
 def get_capabilities(query_string: str, opts: 'Options', window_id: int = 0, os_window_id: int = 0) -> Generator[str, None, None]:
     from .fast_data_types import ERROR_PREFIX
 
-    def result(encoded_query_name: str, x: str | None = None) -> str:
-        if not x:
-            valid = 0 if x is None else 1
-            return f'{valid}+r{encoded_query_name}'
+    def result(encoded_query_name: str, x: str | bool | None = None) -> str:
+        if x is None:
+            return f'0+r{encoded_query_name}'
+        if isinstance(x, bool):
+            return f'{int(x)}+r{encoded_query_name}'
         return f'1+r{encoded_query_name}={hexlify(str(x).encode("utf-8")).decode("ascii")}'
 
     for encoded_query_name in query_string.split(';'):
@@ -543,7 +544,7 @@ def get_capabilities(query_string: str, opts: 'Options', window_id: int = 0, os_
                 yield result(encoded_query_name, rval)
         else:
             if name in bool_capabilities:
-                yield result(encoded_query_name, '')
+                yield result(encoded_query_name, True)
                 continue
             try:
                 val = queryable_capabilities[name]