4c31cb179e0d93cf55a2bd2e9e497d904c2dcc09
Kovid Goyal <kovid@kovidgoyal.net>
2025-03-22 12:05:56 +0530

    Make go bitfield generation code re-useable

---
+++ gen/bitfields.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2025, Kovid Goyal <kovid at kovidgoyal.net>
+
+import os
+from typing import NamedTuple
+
+
+class BitField(NamedTuple):
+     name: str
+     bits: int
+
+
+def typename_for_bitsize(bits: int) -> str:
+     if bits <= 8:
+        return 'uint8'
+     if bits <= 16:
+        return 'uint16'
+     if bits <= 32:
+        return 'uint32'
+     return 'uint64'
+
+
+def make_bitfield(dest: str, typename: str, *fields_: str) -> tuple[str, str]:
+    output_path = os.path.join(dest, f'{typename.lower()}_generated.go')
+    ans = [f'package {os.path.basename(dest)}', '']
+    a = ans.append
+
+    def fieldify(spec: str) -> BitField:
+        name, num = spec.partition(' ')[::2]
+        return BitField(name, int(num))
+
+    fields = tuple(map(fieldify, fields_))
+    total_size = sum(x.bits for x in fields)
+    if total_size > 64:
+        raise ValueError(f'Total size of bit fields: {total_size} for {typename} is larger than 64 bits')
+    a(f'// Total number of bits used: {total_size}')
+    itype = typename_for_bitsize(total_size)
+    a(f'type {typename} {itype}')
+    a('')
+    shift = 0
+    for bf in reversed(fields):
+        tn = typename_for_bitsize(bf.bits)
+        mask = '0b' + '1' * bf.bits
+        a(f'func (s {typename}) {bf.name.capitalize()}() {tn} {{')  # }}
+        if shift:
+            a(f'    return {tn}((s >> {shift}) & {mask})')
+        else:
+            a(f'    return {tn}(s & {mask})')
+        a('}')
+        a('')
+        a(f'func (s *{typename}) Set{bf.name.capitalize()}(val {tn}) {{')  # }}
+        if shift:
+            a(f'    *s &^= {mask} << {shift}')
+            a(f'    *s |= {typename}(val&{mask}) << {shift}')
+        else:
+            a(f'    *s &^= {mask}')
+            a(f'    *s |= {typename}(val & {mask})')
+        a('}')
+        a('')
+        shift += bf.bits
+
+    return output_path, '\n'.join(ans)
