c34399e051bc4b584c6288c49de866588f4f2d17
Kovid Goyal <kovid@kovidgoyal.net>
2025-04-25 15:46:48 +0530

    Include wrapper script as comment

--- rsync-and-build.sh
+++ rsync-and-build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-#
-# rsync-and-build.sh
-# Copyright (C) 2025 Kovid Goyal <kovid at kovidgoyal.net>
-#
-# Distributed under terms of the MIT license.
+
+# To be used via a script such as
+: <<'COMMENT'
+export RSYNC_PASSWORD=password
+export BUILDBOT=rsync://useranme@server/path/to/this/directory
+cd ~/kitty-src || exit 1
+
+script=rsync-and-build.sh
+if [[ -e "$script" ]]; then
+    source "$script"
+else
+    rsync -a --include "$script" --exclude '*' "$BUILDBOT" . && source "$script"
+fi
+COMMENT
 
 rsync --info=progress2 -a -zz --delete --force --exclude /bypy/b --exclude '*_generated.*' --exclude '*_generated_test.*' --exclude '/docs/_build' --include '/.github' --exclude '/.*' --exclude '/dependencies' --exclude '/tags' --exclude '__pycache__' --exclude '/kitty/launcher/kitt*' --exclude '/build' --exclude '/dist' --exclude '*.swp' --exclude '*.swo' --exclude '*.so' --exclude '*.dylib' --exclude '*.dSYM' "$BUILDBOT" . && exec ./dev.sh build "$@"
