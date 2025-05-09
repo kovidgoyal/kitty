cca838b952225a387bcd714094dc9f9df4705780
Kovid Goyal <kovid@kovidgoyal.net>
2025-05-09 07:48:56 +0530

    ...

--- kitty/options/definition.py
+++ kitty/options/definition.py
@@ -4183,7 +4183,7 @@ To setup shortcuts to change only the current OS window's font size::
 
     map kitty_mod+f6 change_font_size current 10.0
 
-To setup shortcuts to multiply/divide the font size:
+To setup shortcuts to multiply/divide the font size::
 
     map kitty_mod+f6 change_font_size all *2.0
     map kitty_mod+f6 change_font_size all /2.0
