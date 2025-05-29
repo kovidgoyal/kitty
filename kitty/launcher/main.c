27fdfe6480880a8e5f0dc4290924ef04beed863e
Kovid Goyal <kovid@kovidgoyal.net>
2025-05-29 13:06:47 +0530

    forgot to close write end of pipe in parent

--- kitty/launcher/main.c
+++ kitty/launcher/main.c
@@ -412,6 +412,7 @@ handle_fast_commandline(CLISpec *cli_spec, const char *instance_group_prefix) {
                 // wait until child has done setsid() before exiting so that it doesnt get a SIGHUP,
                 // see: https://github.com/kovidgoyal/kitty/issues/8680
                 char buf[4];
+                errno = 0; while (close(fds[1]) != 0 && errno == EINTR);
                 errno = 0; while(read(fds[0], buf, sizeof(buf)) == -1 && errno == EINTR);
                 exit(0);
             }
