996cf3a6e6178243e46f88a6bba997ebb9289afd
Gheruha <maxgheruha@gmail.com>
2025-07-19 13:32:05 +0300

    Experiment with runner file

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 #!./kitty/launcher/kitty +launch
 # License: GPL v3 Copyright: 2016, Kovid Goyal <kovid at kovidgoyal.net>
 
-import importlib
+import importlib  # me: importlit will let me assign the import to m
+import inspect
 
 
 def main() -> None:
-    m = importlib.import_module('kitty_tests.main')
-    getattr(m, 'main')()
+    m = importlib.import_module("kitty_tests.main")  # me: assigning test import to m
+    tests = [
+        name
+        for name, obj in m.__dict__.items()
+        if inspect.isfunction(obj)
+    ]  # me: getting the tests helpers names
+
+    print("TESTS NAMES:", tests) # printing the test helpers names
+    getattr(m, "main")()  # me: run all tests imported
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
