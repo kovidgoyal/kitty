b92246448dfee03cd52b9aabb77fc68d1ddd9eac
Kovid Goyal <kovid@kovidgoyal.net>
2025-09-29 20:35:12 +0530

    Fix a regression in the previous release that caused the cursor trail to not be hidden properly
    In refactoring the trail shader, I guess I forgot to premultiply the
    output color.
    
    Fixes #9039

--- kitty/trail_fragment.glsl
+++ kitty/trail_fragment.glsl
@@ -12,5 +12,5 @@ void main() {
     float in_x = step(cursor_edge_x[0], frag_pos.x) * step(frag_pos.x, cursor_edge_x[1]);
     float in_y = step(cursor_edge_y[1], frag_pos.y) * step(frag_pos.y, cursor_edge_y[0]);
     opacity *= 1.0f - in_x * in_y;
-    final_color = vec4(trail_color, opacity);
+    final_color = vec4(trail_color * opacity, opacity);
 }
