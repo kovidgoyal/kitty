7191f9805860c3ab9394720e8b2764688efe82ef
uptu <uptu@uptu.dev>
2025-09-29 14:04:13 -0700

    fix(background): fix background image opacity
    modify:
    * `./kitty/bgimage_fragment.glsl`: reimplement image alpha processing;
      since commit `d52f2e7`, background image opacity wasn't respecting the
      `background_opacity` option due to a portion of the image processing
      being deleted, mistakenly making all images 100% opaque.

--- kitty/bgimage_fragment.glsl
+++ kitty/bgimage_fragment.glsl
@@ -1,11 +1,15 @@
 #pragma kitty_include_shader <alpha_blend.glsl>
+#pragma kitty_include_shader <utils.glsl>
 
 uniform sampler2D image;
 uniform vec4 background;
 in vec2 texcoord;
-out vec4 premult_color;
+out vec4 color;
 
 void main() {
-    vec4 color = texture(image, texcoord);
-    premult_color = alpha_blend(color, background);
+    color = texture(image, texcoord);
+    float alpha = color.a * background.a;
+    vec4 premult_color = vec4_premul(color.rgb, alpha);
+    color = vec4(color.rgb, alpha);
+    color = alpha_blend(color, premult_color);
 }
