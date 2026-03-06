6a51abae9f30063fb097e383eb057354d4e5e349
Kovid Goyal <kovid@kovidgoyal.net>
2026-03-06 15:47:10 +0530

    Get drop status updates working

--- kitty/dnd.h
+++ kitty/dnd.h
@@ -11,3 +11,5 @@
 void drop_move_on_child(Window *w, const char **mimes, size_t num_mimes);
 void drop_left_child(Window *w);
 void drop_free_data(Window *w);
+void drop_set_status(Window *w, int operation, const char *payload, size_t payload_sz, bool more);
+size_t drop_update_mimes(Window *w, const char **allowed_mimes, size_t allowed_mimes_count);
