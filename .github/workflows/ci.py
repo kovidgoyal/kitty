c57305addc2a942478bb371b26ca415f0037429c
Kovid Goyal <kovid@kovidgoyal.net>
2026-03-20 10:45:08 +0530

    Ignore dependency CVEs against unreleased versions of deps

--- .github/workflows/ci.py
+++ .github/workflows/ci.py
@@ -225,8 +225,11 @@ IGNORED_DEPENDENCY_CVES = [
     'CVE-2025-12781',
     'CVE-2025-11468',
     'CVE-2026-2297',
+    'CVE-2026-3644',
+    'CVE-2026-4224',
     # github.com/nwaples/rardecode/v2
     'CVE-2025-11579', # rardecode is version 2.2.1, not vulnerable
+    'CVE-2026-2673',  # openssl fix not released
 ]
 
 
