9c8a4ad540192d6508ab38dbef44e1e44f6520bc
Kovid Goyal <kovid@kovidgoyal.net>
2026-04-04 11:47:08 +0530

    ...

--- kitty/launch.py
+++ kitty/launch.py
@@ -1111,6 +1111,7 @@ def remote_edit(msg: str, window: Window) -> None:
     c.opts.source_window = c.opts.next_to = f'id:{window.id}'
     # We ignore env vars as some editors execute code present in env vars such as VIMINIT
     c.opts.env = ()
+    c.opts.copy_env = False
     w = launch(get_boss(), c.opts, cmdline)
     if w is not None:
         c.source_window_id = window.id
