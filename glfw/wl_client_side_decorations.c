aedb75d4f3029a8d94b2316a66bc9cc86095e590
MiguelRegueiro <miguelpr4242@gmail.com>
2026-07-06 21:07:01 +0200

    Fix FreeBSD Wayland build

--- glfw/wl_client_side_decorations.c
+++ glfw/wl_client_side_decorations.c
@@ -12,6 +12,16 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+// Needed for the BTN_* defines
+#ifdef __has_include
+#if __has_include(<linux/input.h>)
+#include <linux/input.h>
+#elif __has_include(<dev/evdev/input.h>)
+#include <dev/evdev/input.h>
+#endif
+#else
+#include <linux/input.h>
+#endif
 
 #define decs window->wl.decorations
 #define debug debug_rendering
