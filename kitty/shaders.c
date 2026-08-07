af0c6b9efb034fac5bd1fd88c6606b2c04a4b2ed
Kovid Goyal <kovid@kovidgoyal.net>
2026-08-07 17:15:15 +0530

    micro optimisation

--- kitty/shaders.c
+++ kitty/shaders.c
@@ -2397,10 +2397,10 @@ run_custom_end_shader(OSWindow *os_window, float sx, float sy, monotonic_t now) 
     d->mouse_pos[1] = vph > 0.f ? 1.f - (float)os_window->mouse_y / vph : 0.f;
     d->mouse_pos[2] = vph > 0.f ? (float)os_window->mouse_left_press_x / vpw : 0.f;
     d->mouse_pos[3] = vph > 0.f ? 1.f - (float)os_window->mouse_left_press_y / vph : 0.f;
-    d->mouse_button_pressed[0] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_LEFT] ? 1.f : 0.f;
-    d->mouse_button_pressed[1] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_RIGHT] ? 1.f : 0.f;
-    d->mouse_button_pressed[2] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_MIDDLE] ? 1.f : 0.f;
-    d->mouse_button_pressed[3] = os_window->mouse_button_pressed[3] ? 1.f : 0.f;
+    d->mouse_button_pressed[0] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_LEFT];
+    d->mouse_button_pressed[1] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_RIGHT];
+    d->mouse_button_pressed[2] = os_window->mouse_button_pressed[GLFW_MOUSE_BUTTON_MIDDLE];
+    d->mouse_button_pressed[3] = os_window->mouse_button_pressed[3];
     d->mouse_pointer_hidden = is_mouse_hidden(os_window) ? 1.f : 0.f;
     d->timestamp = ((float)monotonic_t_to_ms(now)) / 1e3f;
     d->last_rendered_at = ((float)monotonic_t_to_ms(os_window->last_rendered_at)) / 1e3f;
