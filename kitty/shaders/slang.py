3afa647a9b7ac696e49c00fefdd64374f8bf777a
Kovid Goyal <kovid@kovidgoyal.net>
2026-08-08 13:52:00 +0530

    ...

--- kitty/shaders/slang.py
+++ kitty/shaders/slang.py
@@ -1428,7 +1428,6 @@ public float4 {entry_point}(
     # inactive animated groups do not cause an extra blit draw call.
     pipeline_parts: list[str] = []
     ep_idx = 0
-    num_groups = len(pipeline['groups'])
     for g_idx, group in enumerate(pipeline['groups']):
         n = len(group['shaders'])
         calls = '\n'.join(f'        color = fragment_main{ep_idx + j}(color, t, csd, viewport, animation_progress);' for j in range(n))
@@ -1487,7 +1486,13 @@ VertexOutput vmain_wrap(uint vertex_id : SV_VertexID) {
 }
 
 [shader("fragment")]
-float4 fmain_wrap(float2 texcoord : TEXCOORD, uniform int group, uniform float4 viewport, uniform float animation_progress, uniform bool convert_to_srgb) : SV_Target {
+float4 fmain_wrap(
+    float2 texcoord : TEXCOORD,
+    uniform int group,
+    uniform float4 viewport,
+    uniform float animation_progress,
+    uniform bool convert_to_srgb
+) : SV_Target {
     return pipeline_fragment_main(texcoord, group, viewport, animation_progress, convert_to_srgb);
 }
         """.replace('MODULE', slot.replace('-', '_')).encode()
