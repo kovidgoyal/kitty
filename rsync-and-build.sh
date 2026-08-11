5bb1a19465b54f193e54f9bf619a5027bf9fafe8
Kovid Goyal <kovid@kovidgoyal.net>
2026-08-11 14:02:47 +0530

    Exclude shaders from rsync

--- rsync-and-build.sh
+++ rsync-and-build.sh
@@ -15,4 +15,4 @@ else
 fi
 COMMENT
 
-rsync --info=progress2 -a -zz --delete --force --exclude /bypy/b --exclude '*_generated.*' --exclude '*_generated_test.*' --exclude '/docs/_build' --include '/.github' --exclude '/.*' --exclude '/dependencies' --exclude '/tags' --exclude '__pycache__' --exclude '/kitty/launcher/kitt*' --exclude '/build' --exclude '/dist' --exclude '*.swp' --exclude '*.swo' --exclude '*.so' --exclude '*.dylib' --exclude '*.dSYM' "$BUILDBOT" . && exec ./dev.sh build "$@"
+rsync --info=progress2 -a -zz --delete --force --exclude /bypy/b --exclude '*_generated.*' --exclude '*_generated_test.*' --exclude '/docs/_build' --include '/.github' --exclude '/.*' --exclude '/dependencies' --exclude '/tags' --exclude '__pycache__' --exclude '/kitty/launcher/kitt*' --exclude '/build' --exclude '/dist' --exclude '/shaders' --exclude '*.swp' --exclude '*.swo' --exclude '*.so' --exclude '*.dylib' --exclude '*.dSYM' "$BUILDBOT" . && exec ./dev.sh build "$@"
