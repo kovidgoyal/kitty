c655a592f9e09f863cd43acccf2eec00e1dea2d9
Jesse Miller <jmiller@jmiller.com>
2026-09-08 12:00:09 -0600

    Add shaped run cache
    Per-font verstable map of exact run keys to glyph infos, positions and
    cell groups. Skip runs longer than 32 cells. Full-drop at 2048 entries
    or 1 MiB of live storage.
    
    The intention here is to avoid shaping the same runs over and over again
    on an active screen. This used to not have much of an impact, but now
    "modern" full screen TUIs are actively drawing the screens in such a way
    that harfbuzz shaping is showing up as a more significant portion in
    profiling runs.
    
    The cache is sized to not be a long-lived history but able to cache most
    repeated runs on a few active screens.

---
+++ kitty/shaped-run-cache.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2026 Kovid Goyal <kovid at kovidgoyal.net>
+ *
+ * Distributed under terms of the GPL3 license.
+ */
+
+#pragma once
+
+#include "line.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include <hb.h>
+#pragma GCC diagnostic pop
+
+typedef struct ShapeGroup {
+    unsigned int first_glyph_idx, first_cell_idx, num_glyphs, num_cells;
+    bool has_special_glyph, started_with_infinite_ligature;
+} ShapeGroup;
+
+typedef struct ShapedRun {
+    const hb_glyph_info_t *info;
+    const hb_glyph_position_t *positions;
+    const ShapeGroup *groups;
+    unsigned num_glyphs, num_groups;
+} ShapedRun;
+
+typedef struct {
+    int x;
+} *SHAPED_RUN_MAP_HANDLE;
+
+SHAPED_RUN_MAP_HANDLE
+create_shaped_run_hash_table(void);
+void free_shaped_run_hash_table(SHAPED_RUN_MAP_HANDLE *handle);
+bool shaped_run_get(
+    SHAPED_RUN_MAP_HANDLE map,
+    const CPUCell *cells,
+    index_type num_cells,
+    const TextCache *tc,
+    bool disable_ligature,
+    bool force_ltr,
+    uint8_t scale,
+    uint8_t subscale,
+    ShapedRun *result);
+void shaped_run_put(
+    SHAPED_RUN_MAP_HANDLE map,
+    const CPUCell *cells,
+    index_type num_cells,
+    const TextCache *tc,
+    bool disable_ligature,
+    bool force_ltr,
+    uint8_t scale,
+    uint8_t subscale,
+    const hb_glyph_info_t *info,
+    const hb_glyph_position_t *positions,
+    unsigned num_glyphs,
+    const ShapeGroup *groups,
+    unsigned num_groups);
